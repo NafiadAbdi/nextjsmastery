d55a5ba737ec3d4b7f5ea0da437fea6809d14e19
Nafiad <111278128+NafiadAbdi@users.noreply.github.com>
2025-01-28 05:16:44 -0600

    next js application second

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-3xl text-violet-700 font-extralight">Well come to the world next js </h1>
+    <h1 className="text-3xl text-violet-700">Well come to the world next js </h1>
   );
 }
