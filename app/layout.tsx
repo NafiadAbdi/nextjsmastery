1b8de0a9550187b80273ced19aa0079e4b3d8afe
Nafiad <111278128+NafiadAbdi@users.noreply.github.com>
2025-02-18 07:52:24 -0600

    Assets and Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,12 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Dev Flow",
-  description: "A better version of the stack overflow.",
+  title: "Dev Overflow",
+  description:
+    "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+  icons: {
+    icon: "/images/site-logo.svg",
+  },
 };
 
 export default function RootLayout({
