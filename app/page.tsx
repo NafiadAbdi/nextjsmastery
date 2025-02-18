b3d16cc1bf22c1e22dffaec998db0d91a364a612
Nafiad <111278128+NafiadAbdi@users.noreply.github.com>
2025-02-18 04:40:24 -0600

    Tailwind CSS Setup and Theming

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <h1 className="text-3xl text-violet-700 font-blank">
-      Well come to the world next js{" "}
-    </h1>
-  );
+  return <h1>Welcome to the world next js </h1>;
 }
